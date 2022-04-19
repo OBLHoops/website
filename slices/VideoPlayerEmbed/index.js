17559387c26fdf2c1393819b7a9c5cb789bb65f7
Nathan Searles <nsearles@gmail.com>
2022-04-19 15:55:23 -0700

    updated video player

--- slices/VideoPlayerEmbed/index.js
+++ slices/VideoPlayerEmbed/index.js
@@ -1,6 +1,17 @@
-import React from "react";
-import { PrismicRichText } from "@prismicio/react";
+import ContentContainer from "@components/ContentContainer";
+import styles from "./videoPlayerEmbed.module.scss";
 
-const VideoPlayerEmbed = ({ slice }) => <section>video player</section>;
+const VideoPlayerEmbed = ({ slice }) => {
+  console.log(slice);
+  return (
+    <section className={styles.videoEmbed}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div dangerouslySetInnerHTML={{ __html: slice.primary.videoEmbedLink.html }} />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default VideoPlayerEmbed;
