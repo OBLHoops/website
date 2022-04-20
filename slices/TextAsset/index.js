5e5d2af39bf00af03ebcf2aa35bf728613d3a244
Nathan Searles <nsearles@gmail.com>
2022-04-19 17:17:44 -0700

    updated text asset

--- slices/TextAsset/index.js
+++ slices/TextAsset/index.js
@@ -1,6 +1,21 @@
-import React from "react";
 import { PrismicRichText } from "@prismicio/react";
+import ContentContainer from "@components/ContentContainer";
+import Picture from "@components/Picture";
+import { classNames } from "@lib/utilities";
+import styles from "./textAsset.module.scss";
 
-const TextAsset = ({ slice }) => <section>text + asset</section>;
+const TextAsset = ({ slice }) => {
+  console.log("slice.primary: ", slice.primary);
+
+  const { image, imagePosition, imageOverlap, text, label, title, theme } = slice.primary;
+  return (
+    <section className={classNames([styles.textAsset, styles[`theme-${theme}`]])}>
+      <ContentContainer>
+        {text && <PrismicRichText field={text} />}
+        {image && <Picture image={image} />}
+      </ContentContainer>
+    </section>
+  );
+};
 
 export default TextAsset;
