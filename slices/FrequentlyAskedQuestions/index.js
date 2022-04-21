970b35d916398ec1ace83a9d2075cf33788e4b09
Nathan Searles <nsearles@gmail.com>
2022-04-21 11:30:07 -0700

    roughed in FAQ, added reachui accordion

--- slices/FrequentlyAskedQuestions/index.js
+++ slices/FrequentlyAskedQuestions/index.js
@@ -1,6 +1,67 @@
 import React from "react";
 import { PrismicRichText } from "@prismicio/react";
+import { Accordion, AccordionItem, AccordionButton, AccordionPanel } from "@reach/accordion";
+import "@reach/accordion/styles.css";
+import { v4 as uuidv4 } from "uuid";
+import ContentContainer from "@components/ContentContainer";
+import { classNames } from "@lib/utilities";
+import styles from "./faq.module.scss";
 
-const FrequentlyAskedQuestions = ({ slice }) => <section>Frequently Asked Questions</section>;
+const FrequentlyAskedQuestions = ({ slice }) => {
+  console.log(slice);
+  const { primary, items } = slice;
+
+  const Item = ({ question, answer }) => (
+    <AccordionItem>
+      <h3>
+        <AccordionButton>
+          {question}
+          <svg
+            viewBox="0 0 45 45"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className={styles.icon}
+          >
+            <circle cx="22.5" cy="22.5" r="22.5" fill="black" />
+            <path
+              d="M12 22.04H32.0802"
+              stroke="white"
+              strokeWidth="2.72826"
+              strokeMiterlimit="10"
+              className={styles.iconX}
+            />
+            <path
+              d="M22.04 12V32.0802"
+              stroke="white"
+              strokeWidth="2.72826"
+              strokeMiterlimit="10"
+              className={styles.iconY}
+            />
+          </svg>
+        </AccordionButton>
+      </h3>
+      <AccordionPanel>
+        <PrismicRichText field={answer} />
+      </AccordionPanel>
+    </AccordionItem>
+  );
+
+  return (
+    <section className={classNames([styles.faq])}>
+      <ContentContainer>
+        <PrismicRichText field={primary.title} />
+        <PrismicRichText field={primary.description} />
+
+        {items && (
+          <Accordion>
+            {items.map((item) => (
+              <Item {...item} key={uuidv4()} />
+            ))}
+          </Accordion>
+        )}
+      </ContentContainer>
+    </section>
+  );
+};
 
 export default FrequentlyAskedQuestions;
