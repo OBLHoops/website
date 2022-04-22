8e44d9c7482ebdb512a007b67a5c542baf943e66
Nathan Searles <nsearles@gmail.com>
2022-04-22 09:42:32 -0700

    updated image slice

--- slices/Image/index.js
+++ slices/Image/index.js
@@ -1,6 +1,19 @@
-import React from "react";
-import { PrismicRichText } from "@prismicio/react";
+import Picture from "@components/Picture";
+import ContentContainer from "@components/ContentContainer";
+import styles from "./image.module.scss";
 
-const Image = ({ slice }) => <section>image</section>;
+const Image = ({ slice }) => {
+  const { image } = slice.primary;
+  if (image) {
+    return (
+      <section className={styles.image}>
+        <ContentContainer>
+          <Picture image={image} />
+        </ContentContainer>
+      </section>
+    );
+  }
+  return null;
+};
 
 export default Image;
