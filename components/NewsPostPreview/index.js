8f492c7bd1eca6d1efcabc0287a222512400bfa9
Nathan Searles <nsearles@gmail.com>
2022-04-22 16:39:55 -0700

    updated latest news and news preview posts

--- components/NewsPostPreview/index.js
+++ components/NewsPostPreview/index.js
@@ -1,17 +1,21 @@
 import Link from "next/link";
+import { PrismicLink } from "@prismicio/react";
 import Picture from "@components/Picture";
 import styles from "./newsPostPreview.module.scss";
 
 export default function NewsPostPreview({ slug, data }) {
   return (
     <div className={styles.preview}>
-      <Link href={`/news/${slug}`} scroll={false}>
-        <a>
-          {/* todo add placeholder image */}
-          <Picture image={data.coverImage} />
-          {data.title && <h3>{data.title}</h3>}
-          {data.postData && <p>{data.postData}</p>}
-          {data.source && <p>{data.source}</p>}
+      <Link href={data.externalLink.link_type == "Web" ? data.externalLink.url : `/news/${slug}`}>
+        <a target={data.externalLink.link_type == "Web" ? "_blank" : "_self"}>
+          <div className={styles.image}>
+            {data.coverImage.url && <Picture image={data.coverImage} />}
+          </div>
+          <div className={styles.content}>
+            {data.source && <p className={styles.source}>{data.source}</p>}
+            {data.title && <h3>{data.title}</h3>}
+            {data.postDate && <p className={styles.date}>{data.postDate}</p>}
+          </div>
         </a>
       </Link>
     </div>
--- slices/LatestNews/index.js
+++ slices/LatestNews/index.js
@@ -1,6 +1,50 @@
-import React from "react";
+import { createClient } from "@root/prismicio";
+import { usePrismicDocumentsByType } from "@prismicio/react";
 import { PrismicRichText } from "@prismicio/react";
+import Link from "next/link";
+import { classNames } from "@lib/utilities";
+import NewsPostPreview from "@components/NewsPostPreview";
+import styles from "./latestNews.module.scss";
+const client = createClient();
 
-const LatestNews = ({ slice }) => <section>latest news</section>;
+const LatestNews = ({ slice }) => {
+  const [documents, { state, error }] = usePrismicDocumentsByType("news-post", {
+    client: client,
+    orderings: {
+      field: "my.news-post.postDate",
+      direction: "desc"
+    },
+    pageSize: 3,
+    page: 1
+  });
+  if (documents?.results) {
+    return (
+      <section className={styles.latestNews}>
+        <div className={styles.container}>
+          <div className={styles.intro}>
+            <div>
+              <h2>{slice.primary.label}</h2>
+              <PrismicRichText field={slice.primary.title} />
+            </div>
+            <div>
+              <Link href="/news">
+                <a className={classNames([styles.button, styles.fill])}>
+                  {slice.primary.callToAction ? slice.primary.callToAction : "View all"}
+                </a>
+              </Link>
+            </div>
+          </div>
+
+          <div className={styles.grid}>
+            {documents.results.map((item) => (
+              <NewsPostPreview {...item} slug={item.uid} key={item.id} />
+            ))}
+          </div>
+        </div>
+      </section>
+    );
+  }
+  return null;
+};
 
 export default LatestNews;
