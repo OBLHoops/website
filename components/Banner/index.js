eeaf5afe29c707e55764026e07901d9e34dfcf61
Nathan Searles <nsearles@gmail.com>
2022-04-25 16:37:24 -0700

    updated banner

--- components/Banner/index.js
+++ components/Banner/index.js
@@ -1,12 +1,18 @@
-import Link from "next/link";
+import { PrismicLink } from "@prismicio/react";
 import styles from "./banner.module.scss";
 
 export default function Banner({ bannerData }) {
-  if (bannerData?.data) {
+  console.log(bannerData);
+  if (bannerData?.data && bannerData?.data.active) {
     return (
       <div className={styles.banner}>
         <div className={styles.container}>
-          <p>{bannerData.data.text}</p>
+          <PrismicLink
+            field={bannerData?.data.link}
+            target={bannerData?.data.link.link_type == "Web" ? "_blank" : "_self"}
+          >
+            <p>{bannerData.data.text}</p>
+          </PrismicLink>
         </div>
       </div>
     );
