b407662d1c63331f3c4d785587c9fc3531dab937
Nathan Searles <nsearles@gmail.com>
2022-04-26 09:33:45 -0700

    updated news dates

--- components/NewsPostPreview/index.js
+++ components/NewsPostPreview/index.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import { PrismicLink } from "@prismicio/react";
+import * as prismicH from "@prismicio/helpers";
 import Picture from "@components/Picture";
 import styles from "./newsPostPreview.module.scss";
 
 export default function NewsPostPreview({ slug, data }) {
+  const options = { month: "long", day: "numeric", year: "numeric" };
   return (
     <div className={styles.preview}>
       <Link href={data.externalLink.link_type == "Web" ? data.externalLink.url : `/news/${slug}`}>
@@ -14,7 +16,11 @@ export default function NewsPostPreview({ slug, data }) {
           <div className={styles.content}>
             {data.source && <p className={styles.source}>{data.source}</p>}
             {data.title && <h3>{data.title}</h3>}
-            {data.postDate && <p className={styles.date}>{data.postDate}</p>}
+            {data.postDate && (
+              <p className={styles.date}>
+                {prismicH.asDate(data.postDate).toLocaleString(undefined, options)}
+              </p>
+            )}
           </div>
         </a>
       </Link>
