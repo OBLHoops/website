cc53c23cc2578a1bb0f22c71bc810cfe2f37f811
Nathan Searles <nsearles@gmail.com>
2022-04-28 16:54:53 -0700

    added news filter

---
+++ components/NewsPostResults/index.js
@@ -0,0 +1,46 @@
+import { useEffect, useState, useRef } from "react";
+import { createClient } from "@root/prismicio";
+import { usePrismicDocumentsByType } from "@prismicio/react";
+import NewsPostPreview from "@components/NewsPostPreview";
+const client = createClient();
+const newsPostPerPage = 1;
+
+export default function NewsPostResults({ filterBy, resultsPage, updatePostPages }) {
+  const [postResults, setPostResults] = useState([]);
+  // fetchQuery: only pass document.tags to query if filterBy value exists
+  const fetchQuery = {
+    type: `[at(document.type, "news-post")]`,
+    // not: `[not(document.id,"${pinnedPostUID}")]`,
+    ...(filterBy && { filterBy: `[at(document.tags,["${filterBy}"])]` })
+  };
+
+  const [documents, { state, error }] = usePrismicDocumentsByType("news-post", {
+    client: client,
+    orderings: {
+      field: "my.news-post.postDate",
+      direction: "desc"
+    },
+    q: `[${Object.values(fetchQuery).join("")}]`,
+    pageSize: newsPostPerPage,
+    page: resultsPage
+  });
+
+  useEffect(() => {
+    function getUniqueListBy(arr, key) {
+      return [...new Map(arr.map((item) => [item[key], item])).values()];
+    }
+    if (documents?.results && state === "loaded") {
+      updatePostPages(documents?.total_results_size);
+      setPostResults((currentPostResults) =>
+        getUniqueListBy([...currentPostResults, ...documents.results], "id")
+      );
+    }
+  }, [documents, state]);
+
+  return (
+    <>
+      {postResults &&
+        postResults.map((post) => <NewsPostPreview {...post} slug={post.uid} key={post.id} />)}
+    </>
+  );
+}
