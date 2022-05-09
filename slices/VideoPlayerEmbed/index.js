1059f24856c7de9e404dd5f5bf96a89b1385967b
Nathan Searles <nsearles@gmail.com>
2022-05-09 12:05:09 -0700

    a11y youtube embed

--- slices/VideoPlayerEmbed/index.js
+++ slices/VideoPlayerEmbed/index.js
@@ -2,20 +2,37 @@ import { classNames } from "@lib/utilities";
 import styles from "./videoPlayerEmbed.module.scss";
 
 const VideoPlayerEmbed = ({ slice }) => {
-  return (
-    <section
-      className={classNames([
-        styles.videoEmbed,
-        slice.primary.spacing && styles[`spacing-${slice.primary.spacing}`]
-      ])}
-    >
-      <div className={styles.container}>
-        <div className={styles.wrapper}>
-          <div dangerouslySetInnerHTML={{ __html: slice.primary.videoEmbedLink.html }} />
+  if (slice.primary?.videoEmbedLink?.embed_url) {
+    const parsedUrl = new URL(slice.primary.videoEmbedLink.embed_url);
+    const youTubeEmbedId = parsedUrl.searchParams.get("v");
+    return (
+      <section
+        className={classNames([
+          styles.videoEmbed,
+          slice.primary.spacing && styles[`spacing-${slice.primary.spacing}`]
+        ])}
+      >
+        <div className={styles.container}>
+          <div className={styles.wrapper}>
+            {slice.primary.videoEmbedLink.provider_name === "YouTube" ? (
+              <iframe
+                width={slice.primary.videoEmbedLink.width}
+                height={slice.primary.videoEmbedLink.height}
+                src={`https://www.youtube.com/embed/${youTubeEmbedId}?disablekb=1`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title={slice.primary.videoEmbedLink.title}
+              />
+            ) : (
+              <div dangerouslySetInnerHTML={{ __html: slice.primary.videoEmbedLink.html }} />
+            )}
+          </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
+  return null;
 };
 
 export default VideoPlayerEmbed;
