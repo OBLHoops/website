e95c521ad692e071448c8dda33bcd41cee64e53a
Nathan Searles <nsearles@gmail.com>
2022-05-11 13:44:50 -0700

    fixes OBLW-18 - footer focus

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -8,7 +8,6 @@ import Overlay from "@components/Overlay";
 
 export default function Layout({ children, preview }) {
   const router = useRouter();
-
   const route = router.asPath === "/" ? "homepage" : router.asPath;
   const childProps = React.Children.map(children?.props?.children, (child) => {
     return {
@@ -23,8 +22,10 @@ export default function Layout({ children, preview }) {
       {childProps && <Banner bannerData={childProps[0].bannerData} />}
       {childProps && <Header navData={childProps[0].navData} />}
       {children}
-      {childProps && <Footer footerData={childProps[0].footerData} />}
-      <Overlay key={route} />
+      {childProps && <Footer footerData={childProps[0].footerData} key={route} />}
+      <div>
+        <Overlay key={route} />
+      </div>
       <div hidden>
         <span id="new-window-0">Opens in a new window</span>
         <span id="new-window-1">Opens an external site</span>
