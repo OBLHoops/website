f91dbb4f1f12dba2e34a67cee1d21d73118bb265
Nathan Searles <nsearles@gmail.com>
2022-05-13 10:10:40 -0700

    added latest news data caching with SWR

--- slices/LatestNews/index.js
+++ slices/LatestNews/index.js
@@ -1,17 +1,16 @@
+import { useEffect } from "react";
 import { createClient } from "@root/prismicio";
 import { usePrismicDocumentsByType } from "@prismicio/react";
 import { PrismicRichText } from "@prismicio/react";
 import Link from "next/link";
-import dynamic from "next/dynamic";
+import NewsPostPreview from "@components/NewsPostPreview";
+import { useSharedState } from "@lib/useSharedState";
 import { classNames } from "@lib/utilities";
 import styles from "./latestNews.module.scss";
-
-const NewsPostPreview = dynamic(() => import("@components/NewsPostPreview"), { ssr: false });
-
 const client = createClient();
 
 const LatestNews = ({ slice }) => {
-  const [documents, { state, error }] = usePrismicDocumentsByType("news-post", {
+  const [documents] = usePrismicDocumentsByType("news-post", {
     client: client,
     orderings: {
       field: "my.news-post.postDate",
@@ -20,34 +19,39 @@ const LatestNews = ({ slice }) => {
     pageSize: 3,
     page: 1
   });
-  if (documents?.results) {
-    return (
-      <section className={styles.latestNews}>
-        <div className={styles.container}>
-          <div className={styles.intro}>
-            <div>
-              <h2>{slice.primary.label}</h2>
-              <PrismicRichText field={slice.primary.title} />
-            </div>
-            <div>
-              <Link href="/news">
-                <a className={classNames([styles.button, styles.fill])}>
-                  {slice.primary.callToAction ? slice.primary.callToAction : "View all"}
-                </a>
-              </Link>
-            </div>
-          </div>
 
-          <div className={styles.grid}>
-            {documents.results.map((item) => (
-              <NewsPostPreview {...item} slug={item.uid} key={item.id} />
-            ))}
+  const [postsData, setPostsData] = useSharedState("postsData", documents);
+
+  useEffect(() => {
+    documents?.results && setPostsData(documents);
+  }, [documents, setPostsData]);
+
+  return (
+    <section className={styles.latestNews}>
+      <div className={styles.container}>
+        <div className={styles.intro}>
+          <div>
+            <h2>{slice.primary.label}</h2>
+            <PrismicRichText field={slice.primary.title} />
+          </div>
+          <div>
+            <Link href="/news">
+              <a className={classNames([styles.button, styles.fill])}>
+                {slice.primary.callToAction ? slice.primary.callToAction : "View all"}
+              </a>
+            </Link>
           </div>
         </div>
-      </section>
-    );
-  }
-  return null;
+
+        <div className={styles.grid}>
+          {postsData?.results &&
+            postsData.results.map((item) => (
+              <NewsPostPreview {...item} slug={item.uid} key={item.id} />
+            ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default LatestNews;
