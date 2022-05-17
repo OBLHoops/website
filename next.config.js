a0c3176bd78e29e61511a90a55c916d15eb0f1eb
Nathan Searles <nsearles@gmail.com>
2022-05-16 17:19:15 -0700

    Updated security headers and content security policy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,79 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const isDev = process.env.NODE_ENV !== "production";
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true
+};
+
+const ContentSecurityPolicy = `
+  default-src 'none';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' *.prismic.io https://prismic.io https://www.googletagmanager.com;
+  prefetch-src 'self' *.vercel.app;
+  connect-src 'self' boards-api.greenhouse.io *.prismic.io;
+  object-src 'none';
+  style-src 'self' 'unsafe-inline' fonts.googleapis.com;
+  media-src 'none';
+  img-src 'self' *.prismic.io www.googletagmanager.com data:;
+  font-src 'self' fonts.googleapis.com fonts.gstatic.com data:;
+  frame-src 'self' *.prismic.io *.youtube.com *.vimeo.com *.typeform.com;
+  manifest-src 'self'
+`;
+
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim()
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on"
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block"
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: "X-Frame-Options",
+    value: "DENY"
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Optio
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff"
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains; preload"
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin"
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  // Opt-out of Google FLoC: https://amifloced.org/
+  {
+    key: "Permissions-Policy",
+    value: "geolocation=(), interest-cohort=()"
+  }
+];
+
+module.exports = {
+  async headers() {
+    if (isDev) {
+      return [
+        {
+          source: "/:path*",
+          headers: securityHeaders
+        }
+      ];
+    }
+    return [];
+  },
+  nextConfig
+};
