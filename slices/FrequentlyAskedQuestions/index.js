bf2d7f672301c63426cde4f67a71adad78773611
Nathan Searles <nsearles@gmail.com>
2022-06-24 11:24:20 -0700

    replaced reach-ui with headlessui

--- slices/FrequentlyAskedQuestions/index.js
+++ slices/FrequentlyAskedQuestions/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { PrismicRichText } from "@prismicio/react";
-import { Accordion, AccordionItem, AccordionButton, AccordionPanel } from "@reach/accordion";
-import "@reach/accordion/styles.css";
+import { Disclosure } from "@headlessui/react";
+
 import { v4 as uuidv4 } from "uuid";
 import ContentContainer from "@components/ContentContainer";
 import { classNames } from "@lib/utilities";
@@ -11,38 +11,42 @@ const FrequentlyAskedQuestions = ({ slice }) => {
   const { primary, items } = slice;
 
   const Item = ({ question, answer }) => (
-    <AccordionItem>
-      <h3>
-        <AccordionButton>
-          <div>{question}</div>
-          <svg
-            viewBox="0 0 45 45"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className={styles.icon}
-          >
-            <circle cx="22.5" cy="22.5" r="22.5" fill="black" />
-            <path
-              d="M12 22.04H32.0802"
-              stroke="white"
-              strokeWidth="2.72826"
-              strokeMiterlimit="10"
-              className={styles.iconX}
-            />
-            <path
-              d="M22.04 12V32.0802"
-              stroke="white"
-              strokeWidth="2.72826"
-              strokeMiterlimit="10"
-              className={styles.iconY}
-            />
-          </svg>
-        </AccordionButton>
-      </h3>
-      <AccordionPanel>
-        <PrismicRichText field={answer} />
-      </AccordionPanel>
-    </AccordionItem>
+    <Disclosure as="div" className={styles.item}>
+      {({ open }) => (
+        <>
+          <h3>
+            <Disclosure.Button className={styles.button}>
+              <div>{question}</div>
+              <svg
+                viewBox="0 0 45 45"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className={styles.icon}
+              >
+                <circle cx="22.5" cy="22.5" r="22.5" fill="black" />
+                <path
+                  d="M12 22.04H32.0802"
+                  stroke="white"
+                  strokeWidth="2.72826"
+                  strokeMiterlimit="10"
+                  className={styles.iconX}
+                />
+                <path
+                  d="M22.04 12V32.0802"
+                  stroke="white"
+                  strokeWidth="2.72826"
+                  strokeMiterlimit="10"
+                  className={classNames([styles.iconY, open && styles.open])}
+                />
+              </svg>
+            </Disclosure.Button>
+          </h3>
+          <Disclosure.Panel className={styles.panel}>
+            <PrismicRichText field={answer} />
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
   if (items[0]?.question && items[0]?.answer.length) {
     return (
@@ -51,13 +55,7 @@ const FrequentlyAskedQuestions = ({ slice }) => {
           <PrismicRichText field={primary.title} />
           <PrismicRichText field={primary.description} />
 
-          {items && (
-            <Accordion collapsible>
-              {items.map((item) => (
-                <Item {...item} key={uuidv4()} />
-              ))}
-            </Accordion>
-          )}
+          {items && items.map((item) => <Item {...item} key={uuidv4()} />)}
         </ContentContainer>
       </section>
     );
