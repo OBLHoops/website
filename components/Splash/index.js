799fce11518468756f576c1217bda296d6d66e04
Nathan Searles <nsearles@gmail.com>
2022-06-28 14:03:57 -0700

    inlined splash css

--- components/Splash/index.js
+++ components/Splash/index.js
@@ -1,5 +1,4 @@
 import { motion } from "framer-motion";
-import styles from "./splash.module.scss";
 
 export default function Splash() {
   return (
@@ -9,7 +8,16 @@ export default function Splash() {
         opacity: 0
       }}
       transition={{ duration: 0.85, delay: 0.25, ease: "easeInOut" }}
-      className={styles.splash}
+      style={{
+        zIndex: 5000,
+        position: "fixed",
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        backgroundColor: "#000",
+        opacity: 1
+      }}
     ></motion.div>
   );
 }
